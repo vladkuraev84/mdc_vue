7b132a07650f1e7c598353787774bf0e8af935c5
vladkuraev84 <vladkuraev84@gmail.com>
2021-03-17 20:06:35 +0200

    feed

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,7 +1,38 @@
 <template>
-  <div>feed {{ apiUrlFeed }}</div>
+  <div>
+    <div v-if="isLoading">Loading...</div>
+    <div v-if="error">Smth bad happened</div>
+    <div v-if="feed">
+      <div class="article-preview"
+        v-for="(article, index) in feed.articles" :key="index"
+      >
+        <div class="article-meta">
+          <router-link :to="{name: 'userProfile', params: {slug: article.author.username}}">
+            <img :src="article.author.image" alt="" />
+          </router-link>
+          <div class="info">
+            <router-link :to="{name: 'userProfile', params: {slug: article.author.username}}" class="author">
+              {{ article.author.username }}
+            </router-link>
+            <span class="date">{{ article.createdAt }}</span>
+          </div>
+          <div class="pull-xs-right">
+            ADD TO FAVORITE
+          </div>
+        </div>
+        <router-link :to="{name: 'article', params: {slug: article.slug}}" class="preview-link">
+          <span class="title" style="display:block; color: #000000; font-size: 24px;font-weight: 900;">{{article.title}}</span>
+          <span style="display:inline-block;width: 100%;">{{ article.description }}</span>
+          <span>Read more...</span>
+          TAG LIST
+        </router-link>
+      </div>
+      Pagination
+    </div>
+  </div>
 </template>
 <script>
+import {mapState} from 'vuex';
 import {actionTypes} from "@/store/modules/feed";
 
 export default {
@@ -11,6 +42,13 @@ export default {
       type: String,
       required: true
     }
+  },
+  computed: {
+    ...mapState({
+      isLoading: state => state.feed.isLoading,
+      feed: state => state.feed.data,
+      error: state => state.feed.error
+    })
   },
   mounted() {
     console.log('init feed')
